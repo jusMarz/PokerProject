1c3fcfda8ec690ca30a20137ee302456973f43f2
BT_2S14_14 <@>
2025-01-28 10:15:36 -0500

    Intial Commit

--- src/FileClass.java
+++ src/FileClass.java
@@ -1,23 +1,30 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class FileClass
 {
     public static void main(String[] args) {
-    File f = new File("data/InputFile");
-    Scanner s = null;
-    try {
-            Scanner s = new Scanner(f);
-        } catch (FileNotFoundException ex) {
-            throw new RuntimeException(ex);
+        File f = new File("data/InputFile");
+        Scanner s = null;
+
+            try
+            {
+                s = new Scanner(f);
+            } catch (FileNotFoundException ex)
+            {
+                throw new RuntimeException(ex);
+            }
+
+        String fileData = "";
+        while (s.hasNextLine()) {
+            fileData += s.nextLine() + "\n";
         }
 
-
-
-
-
-
+        String[] fileDater = fileData.split("\n");
+       System.out.println(Arrays.toString(fileDater));
+    }
 
 
 }
