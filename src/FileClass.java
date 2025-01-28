45d8d7bd18642c5a844131dd22005acc59679a02
BT_2S14_14 <@>
2025-01-28 10:06:31 -0500

    Intial Commit

--- src/FileClass.java
+++ src/FileClass.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 
 public class FileClass
 {
+    public static void main(String[] args) {
     File f = new File("data/InputFile");
     Scanner s = null;
+    try {
+            Scanner s = new Scanner(f);
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+
+
 
 
 
